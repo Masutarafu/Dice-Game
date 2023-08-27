26f68ee6bcfacabefcbe6289cca0d0a8dfcbd352
FAROOK <112445407+Masutarafu@users.noreply.github.com>
2023-08-27 22:41:45 +0100

    Update index.js
    Testing again. It should be complete now though.

--- JS/index.js
+++ JS/index.js
@@ -2,24 +2,23 @@ const rand1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber1 = rand1;
 
 if(randomNumber1 === 1) {
-    document.querySelector(".img1").setAttribute("src", "dice1.png")
+    document.querySelector(".img1").setAttribute("src", "./images/dice1.png")
 }
 else if(randomNumber1 === 2) {
-    document.querySelector(".img1").setAttribute("src", "/dice2.png")
+    document.querySelector(".img1").setAttribute("src", "./images/dice2.png")
 }
 else if(randomNumber1 === 3) {
-    document.querySelector(".img1").setAttribute("src", "dice3.png")
+    document.querySelector(".img1").setAttribute("src", "./images/dice3.png")
 }
 else if(randomNumber1 === 4) {
-    document.querySelector(".img1").setAttribute("src", "dice4.png")
+    document.querySelector(".img1").setAttribute("src", "./images/dice4.png")
 }
 else if(randomNumber1 === 5) {
-    document.querySelector(".img1").setAttribute("src", "dice5.png")
+    document.querySelector(".img1").setAttribute("src", "./images/dice5.png")
 }
 else{
-    document.querySelector(".img1").setAttribute("src", "dice6.png")
+    document.querySelector(".img1").setAttribute("src", "./images/dice6.png")
 };
-
 
 const rand2 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = rand2;
