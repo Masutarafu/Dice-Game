3362c87376b273699254bef08f47c9eb0c35e269
FAROOK <112445407+Masutarafu@users.noreply.github.com>
2023-08-27 22:09:49 +0100

    Main Commit
    It's most likely the only commit as the code is already finished. 
    Updates might come in the future maybe tweaks and all that.

---
+++ JS/index.js
@@ -0,0 +1,55 @@
+const rand1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = rand1;
+
+if(randomNumber1 === 1) {
+    document.querySelector(".img1").setAttribute("src", "./Images/dice1.png")
+}
+else if(randomNumber1 === 2) {
+    document.querySelector(".img1").setAttribute("src", "./Images/dice2.png")
+}
+else if(randomNumber1 === 3) {
+    document.querySelector(".img1").setAttribute("src", "./Images/dice3.png")
+}
+else if(randomNumber1 === 4) {
+    document.querySelector(".img1").setAttribute("src", "./Images/dice4.png")
+}
+else if(randomNumber1 === 5) {
+    document.querySelector(".img1").setAttribute("src", "./Images/dice5.png")
+}
+else{
+    document.querySelector(".img1").setAttribute("src", "./Images/dice6.png")
+};
+
+
+const rand2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = rand2;
+
+if(randomNumber2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "./Images/dice1.png")
+}
+else if(randomNumber2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "./Images/dice2.png")
+}
+else if(randomNumber2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "./Images/dice3.png")
+}
+else if(randomNumber2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "./Images/dice4.png")
+}
+else if(randomNumber2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "./Images/dice5.png")
+}
+else{
+    document.querySelector(".img2").setAttribute("src", "./Images/dice6.png")
+};
+
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+}
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins"
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw"
+}
